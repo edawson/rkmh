05f988a75b609ae9660a20aa40a1e732560901b6
Eric T Dawson <eric.t.dawson@gmail.com>
2016-09-13 23:18:27 +0100

    Added code to compile HASHTCounter

--- rkmh.cpp
+++ rkmh.cpp
@@ -16,6 +16,7 @@
 #include "kseq.hpp"
 #include "equiv.hpp"
 #include "json.hpp"
+#include "HASHTCounter.hpp"
 
 // for convenience
 using json = nlohmann::json;
@@ -24,6 +25,7 @@ KSEQ_INIT(gzFile, gzread)
 
     using namespace std;
     using namespace mkmh;
+    using namespace HTC;
 
 
 
@@ -214,6 +216,7 @@ void hash_sequences(vector<string>& keys,
         bool doReadDepth,
         bool doReferenceDepth){
 
+
     if (doReadDepth){
         #pragma omp parallel for
         for (int i = 0; i < keys.size(); i++){
@@ -227,6 +230,7 @@ void hash_sequences(vector<string>& keys,
                     for (int j = 0; j < hash_lengths[i]; j++){
                         //#pragma omp atomic update TODO removing this is under testing
                         read_hash_to_depth[hashes[i][j]] ++;
+
                     }
                 }
             }
@@ -259,7 +263,6 @@ void hash_sequences(vector<string>& keys,
         }
 
     }
-
 
 }
 
